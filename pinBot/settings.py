18c0f39623937bfaa8077ffcd397be91a110fa5f
FarhanMubashir <91379596+FarhanMubashir@users.noreply.github.com>
2022-08-04 18:09:42 +0500

    new

--- pinBot/settings.py
+++ pinBot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ph=#)mu8**7jn5tugr@vspax%o3(y4&zu*^$^e!1+lrk*+@i19
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
