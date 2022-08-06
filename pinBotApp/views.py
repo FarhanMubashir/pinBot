406dcad02fc7a01d9b904f5fe793525eaab1a19f
FarhanMubashir <91379596+FarhanMubashir@users.noreply.github.com>
2022-08-06 11:18:03 +0500

    new

--- pinBotApp/views.py
+++ pinBotApp/views.py
@@ -33,21 +33,20 @@ def apicall(request):
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument("window-size=1024,768")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("download.default_directory=C:/Downloads")
     driver = webdriver.Chrome(chrome_options=chrome_options)
 
 
     driver.get("https://www.nmc.org.uk/registration/search-the-register/")
 
     element = driver.find_element('id','PinNumber')
-    time.sleep(2)
+    time.sleep(3)
     element.send_keys(pin)
     loginbtn = driver.find_element('id','searchRegisterButton')
     warning = driver.find_element('id','validationSummary').text
     cook = driver.find_element('id',"CybotCookiebotDialogBodyButtonAccept").click()
-    time.sleep(2)
+    time.sleep(3)
     loginbtn.click()
-    time.sleep(2)
+    time.sleep(3)
 
     if warning == 'please verify you are not a robot':
         loginbtn.click()
@@ -57,25 +56,28 @@ def apicall(request):
         
         clickView = driver.find_element(by=By.CLASS_NAME, value="more-link").click()
 
-        time.sleep(2)
+        time.sleep(3)
 
-        download = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div/div/div/div[2]/ul/li[2]/a')
-        time.sleep(2)
+        download = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div/div/div/div[2]/ul/li[2]/a').get_attribute('href')
+        time.sleep(3)
 
-        download.click()
-        print(download.text,'________download')
+        print(download,'_________________')
+        # download.click()
+        # print(download.text,'________download')
+
 
         time.sleep(3)
 
         name = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div/div/div/div[3]/div[1]/dl[1]/dd').text
 
         context = {
-            'name': name
+            'name': name,
+            'data': download,
         }
 
         print(name,'here it is')
 
-        time.sleep(2)
+        time.sleep(3)
 
         return render(request,'result.html',context)
     
